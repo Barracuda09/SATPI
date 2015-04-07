813cc0e041a227e39f76a1008f8227a8a1c476a3
Barracuda09 <m.a.postema@alice.nl>
2015-04-07 19:04:31 +0000

    Fix compiler error

---
+++ src/Configure.h
@@ -0,0 +1,34 @@
+/* Configure.h
+
+   Copyright (C) 2015 Marc Postema (m.a.postema -at- alice.nl)
+
+   This program is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public License
+   as published by the Free Software Foundation; either version 2
+   of the License, or (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+   Or, point your browser to http://www.gnu.org/copyleft/gpl.html
+*/
+#ifndef CONFIGURE_H_INCLUDE
+#define CONFIGURE_H_INCLUDE
+
+// Class names
+#define CLASS_STREAMPROPERTIES   "StreamProperties"
+
+// Variable names
+#define VARI_DVR_BUFFER          "dvrbuffer"
+#define VARI_DISEQC_REPEAT       "diseqc_repeat"
+
+#define SSDP_INTERVAL_VAR  "ssdp_interval"
+#define SYSLOG_ON_VAR      "syslog_on"
+#define RESET_FRONTEND     "reset_fe"
+
+#endif // CONFIGURE_H_INCLUDE
