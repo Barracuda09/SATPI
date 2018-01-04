f6972b453122af9d94fce2bc6e152a883a588f8a
Marc <m.a.postema@alice.nl>
2018-01-04 21:05:21 +0100

    Adjust Copyright year of source code and Web Interface

--- src/input/Transformation.h
+++ src/input/Transformation.h
@@ -1,6 +1,6 @@
 /* Transformation.h
 
-   Copyright (C) 2015 - 2017 Marc Postema (mpostema09 -at- gmail.com)
+   Copyright (C) 2014 - 2018 Marc Postema (mpostema09 -at- gmail.com)
 
    This program is free software; you can redistribute it and/or
    modify it under the terms of the GNU General Public License
