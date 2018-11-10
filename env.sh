fcf5792308f80ee6963a456c9c3a80cb1f3b5fbc
Marc <m.a.postema@alice.nl>
2018-11-10 13:09:19 +0100

    Add env.sh to compiling for Vu+ enviroment

---
+++ env.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# source ./env.sh
+
+#export INCLUDES=--sysroot=~/git/pi/tools/arm-bcm2708/arm-rpi-4.9.3-linux-gnueabihf/arm-linux-gnueabihf/sysroot
+#export INCLUDES=
+export CXXPREFIX=~/vu/opt/toolchains/stbgcc-4.8-1.7/bin/arm-linux-
+export CXXSUFFIX=
+export CXX=~/vu/opt/toolchains/stbgcc-4.8-1.7/bin/arm-linux-c++
+export CPP=~/vu/opt/toolchains/stbgcc-4.8-1.7/bin/arm-linux-cpp
+export CC=~/vu/opt/toolchains/stbgcc-4.8-1.7/bin/arm-linux-gcc
+
+PYTHON_CPPFLAGS=~/vu/opt/toolchains/stbgcc-4.8-1.7/python-runtime/include/python2.7
