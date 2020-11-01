abfd0e8613748a4a7ed3daff7b17bf92b48e5e2b
Barracuda09 <mpostema09@gmail.com>
2020-11-01 10:36:42 +0100

    Add Broadcom MIPSEL enviroment script

---
+++ envMIPS.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+# use:
+#    source ./env.sh
+#
+# CFLAGS as used for
+
+###############################################################################
+# MIPSEL based BroadCom Chips
+export CPU_FLAGS=""
+
+export CXXPREFIX=~/vu/opt/toolchains/stbgcc-6.3-1.8/bin/mipsel-linux-
+export CXXSUFFIX=
+export CXX=~/vu/opt/toolchains/stbgcc-6.3-1.8/bin/mipsel-linux-c++
+export CPP=~/vu/opt/toolchains/stbgcc-6.3-1.8/bin/mipsel-linux-cpp
+export CC=~/vu/opt/toolchains/stbgcc-6.3-1.8/bin/mipsel-linux-gcc
+export PYTHON_CPPFLAGS=~/vu/opt/toolchains/stbgcc-6.3-1.8/python-runtime/include/python2.7
+###############################################################################
