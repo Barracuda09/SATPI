e3271878298d3a7bfe57baae0db7cfb283d67369
Barracuda09 <mpostema09@gmail.com>
2021-01-04 16:46:33 +0100

    Update copyright year

--- src/socket/SocketClient.h
+++ src/socket/SocketClient.h
@@ -1,6 +1,6 @@
 /* SocketClient.h
 
-   Copyright (C) 2014 - 2020 Marc Postema (mpostema09 -at- gmail.com)
+   Copyright (C) 2014 - 2021 Marc Postema (mpostema09 -at- gmail.com)
 
    This program is free software; you can redistribute it and/or
    modify it under the terms of the GNU General Public License
