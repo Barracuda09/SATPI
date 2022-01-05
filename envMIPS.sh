a2500bcb65ccb0168a9590e9eae784b34f40bd2a
Barracuda09 <mpostema09@gmail.com>
2022-01-05 13:53:02 +0100

    Fix: Use correct compiler for MIPS cpu

--- envMIPS.sh
+++ envMIPS.sh
@@ -6,20 +6,20 @@
 # CFLAGS as used for
 
 ###############################################################################
-# MIPSEL based BroadCom Chips
+# MIPS based BroadCom Chips
 export CPU_FLAGS=""
 
-#export CXXPREFIX=~/vu/opt/toolchains/stbgcc-6.3-1.8/bin/mipsel-linux-
+#export CXXPREFIX=~/vu/opt/toolchains/stbgcc-6.3-1.8/bin/mips-linux-
 #export CXXSUFFIX=
-#export CXX=~/vu/opt/toolchains/stbgcc-6.3-1.8/bin/mipsel-linux-c++
-#export CPP=~/vu/opt/toolchains/stbgcc-6.3-1.8/bin/mipsel-linux-cpp
-#export CC=~/vu/opt/toolchains/stbgcc-6.3-1.8/bin/mipsel-linux-gcc
+#export CXX=~/vu/opt/toolchains/stbgcc-6.3-1.8/bin/mips-linux-c++
+#export CPP=~/vu/opt/toolchains/stbgcc-6.3-1.8/bin/mips-linux-cpp
+#export CC=~/vu/opt/toolchains/stbgcc-6.3-1.8/bin/mips-linux-gcc
 #export PYTHON_CPPFLAGS=~/vu/opt/toolchains/stbgcc-6.3-1.8/python-runtime/include/python2.7
 
-export CXXPREFIX=~/vu/opt/toolchains/stbgcc-8.3-0.4/bin/mipsel-linux-
+export CXXPREFIX=~/vu/opt/toolchains/stbgcc-8.3-0.4/bin/mips-linux-
 export CXXSUFFIX=
-export CXX=~/vu/opt/toolchains/stbgcc-8.3-0.4/bin/mipsel-linux-c++
-export CPP=~/vu/opt/toolchains/stbgcc-8.3-0.4/bin/mipsel-linux-cpp
-export CC=~/vu/opt/toolchains/stbgcc-8.3-0.4/bin/mipsel-linux-gcc
+export CXX=~/vu/opt/toolchains/stbgcc-8.3-0.4/bin/mips-linux-c++
+export CPP=~/vu/opt/toolchains/stbgcc-8.3-0.4/bin/mips-linux-cpp
+export CC=~/vu/opt/toolchains/stbgcc-8.3-0.4/bin/mips-linux-gcc
 export PYTHON_CPPFLAGS=~/vu/opt/toolchains/stbgcc-8.3-0.4/python-runtime/include/python2.7
 ###############################################################################
