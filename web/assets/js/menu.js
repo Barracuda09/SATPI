2061b268736840ae688b225d1e243fea93dfb6af
Barracuda09 <mpostema09@gmail.com>
2022-02-14 20:22:57 +0100

    Change: Layout and text size of WebIF

--- web/assets/js/menu.js
+++ web/assets/js/menu.js
@@ -6,7 +6,7 @@ function buildmenu() {
 	menu +=     "<span class=\"navbar-toggler-icon\"></span>";
 	menu +=   "</button>";
 	menu +=   "<div class=\"collapse navbar-collapse\" id=\"SatPINavbar\">";
-	menu +=     "<ul class=\"navbar-nav\">";
+	menu +=     "<ul class=\"navbar-nav mr-auto\">";
 	menu +=       "<li class=\"nav-item\" id=\"index\"            ><a class=\"nav-link\" href=\"index.html\"><span class=\"fas fa-home\"></span> Home</a></li>";
 	menu +=       "<li class=\"nav-item\" id=\"log\"              ><a class=\"nav-link\" href=\"log.html\"><span class=\"fas fa-list-alt\"></span> Log</a></li>";
 	menu +=       "<li class=\"nav-item\" id=\"frontendoverview\" ><a class=\"nav-link\" href=\"frontendoverview.html\"><span class=\"fas fa-binoculars\"></span> Frontend Overview</a></li>";
@@ -15,17 +15,10 @@ function buildmenu() {
 	menu +=       "<li class=\"nav-item\" id=\"contact\"          ><a class=\"nav-link\" href=\"contact.html\"><span class=\"fas fa-envelope-open\"></span> Contact</a></li>";
 	menu +=       "<li class=\"nav-item\" id=\"about\"            ><a class=\"nav-link\" href=\"about.html\"><span class=\"fas fa-info-circle\"></span> About</a></li>";
 	menu +=     "</ul>";
+	menu +=     "<span class=\"navbar-text small-text-size\">";
+	menu +=       "Copyright &#169; 2014 - 2022 Marc Postema";
+	menu +=     "</span>";
 	menu +=   "</div>";
-	menu += "</nav>";
-	return menu;
-}
-
-function buildfooter() {
-	var menu = "";
-	menu += "<nav class=\"navbar navbar-expand-sm bg-dark navbar-dark fixed-bottom\">";
-	menu += "  <span class=\"navbar-text\">";
-	menu += "    <p class=\"\">Copyright &#169; 2014 - 2021 Marc Postema</p>";
-	menu += "  </span>";
 	menu += "</nav>";
 	return menu;
 }
