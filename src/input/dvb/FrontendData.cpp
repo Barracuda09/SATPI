a15ffa2526a59ceab59950878b9512870e6e9a7b
Barracuda09 <mpostema09@gmail.com>
2024-01-20 12:35:41 +0100

    Add: fallback to auto modulation (QAM_AUTO) if mtype is not defined

--- src/input/dvb/FrontendData.cpp
+++ src/input/dvb/FrontendData.cpp
@@ -275,11 +275,14 @@ void FrontendData::doParseStreamString(const FeID id, const TransportParamVector
 			_modtype = QAM_128;
 		} else if (mtype == "256qam" || mtype == "qam256") {
 			_modtype = QAM_256;
+		} else if (mtype == "auto") {
+			_modtype = QAM_AUTO;
 		} else {
-			SI_LOG_ERROR("Frontend: @#1, Unknown modulation type [@#2]", id, mtype);
+			SI_LOG_ERROR("Frontend: @#1, Unknown modulation type [@#2] using auto", id, mtype);
+			_modtype = QAM_AUTO;
 		}
 	} else if (msys != input::InputSystem::UNDEFINED) {
-		// no 'mtype' set, so guess one according to 'msys'
+		// no 'mtype' set. But msys is set, so guess 'mtype' according to 'msys'
 		switch (msys) {
 			case input::InputSystem::DVBS:
 				_modtype = QPSK;
