19b05a3b918e00710f955228af53584f0c53436c
Barracuda09 <mpostema09@gmail.com>
2024-01-26 09:13:52 +0100

    Change: path and file name to Broadcom GCC 8.3.0 Tool-Chain

--- envARM.sh
+++ envARM.sh
@@ -12,9 +12,14 @@ if [[ -n $1 && $1 = "gcc12" ]] ; then
   export CXXPREFIX=arm-linux-gnueabihf-
   export CXXSUFFIX=-12
   export CXX=$CXXPREFIX"g++"$CXXSUFFIX
+elif [[ -n $1 && $1 = "gcc4" ]] ; then
+  export CXXPREFIX=~/vu/opt/toolchains/stbgcc-4.8-1.7/bin/arm-linux-gnueabihf-
+  export CXXSUFFIX=
+  export CXX=$CXXPREFIX"c++"$CXXSUFFIX
 else
-  export CXXPREFIX=~/vu/opt/toolchains/stbgcc-8.3-0.4/bin/arm-linux-
+  export CXXPREFIX=~/vu/opt/toolchains/stbgcc-8.3-0.4/bin/arm-linux-gnueabihf-
   export CXXSUFFIX=
   export CXX=$CXXPREFIX"c++"$CXXSUFFIX
 fi
 echo Using: $CXX
+$CXX --version
